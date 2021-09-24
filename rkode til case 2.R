bb14755f1ee0ab631fd3e97297f4bc27cb84e6c0
oysteinm <omy000@uit.no>
2021-09-24 10:06:43 +0200

    changes

--- rkode til case 2.R
+++ rkode til case 2.R
@@ -2,7 +2,7 @@
 #knitr::opts_chunk$set(echo = TRUE)
 #knitr::opts_chunk$set(comment=NA)
 
-
+rm(list=ls())
 ## --------------------------------------------------------------------------------------------------------------------------------
 library(tidyverse)
 library(rvest)
@@ -12,34 +12,37 @@ library(rvest)
 webpage <- read_html("https://www.ssb.no/a/histstat/aarbok/ht-0901-bnp.html")
 tabell <- html_table(html_nodes(webpage, "table")[[2]])
 
+names(tabell)[1] <- c("År")
+
+tabell <- as_tibble(tabell)
+tabell
 
 ## --------------------------------------------------------------------------------------------------------------------------------
-head(tabell)
-tail(tabell)
-str(tabell)
+#head(tabell)
+#tail(tabell)
+#str(tabell)
+#names(tabell)
+
 names(tabell)
+library(janitor)
+
+tabell <- clean_names(tabell)
 
 
 ## --------------------------------------------------------------------------------------------------------------------------------
 tabell <- tabell %>% drop_na()
 
-
 ## --------------------------------------------------------------------------------------------------------------------------------
-names(tabell) <- c("År", "BNP", "BNP_endring",
-                   "BNP_percap", "BNP_percap_endring")
-
-tabell <- as_tibble(tabell)
-
-tabell
-
+#names(tabell) <- c("År", "BNP", "BNP_endring",
+#                   "BNP_percap", "BNP_percap_endring")
 
 ## --------------------------------------------------------------------------------------------------------------------------------
 tabell <-
   tabell %>% 
-  mutate(BNP=str_replace_all(BNP, " ", ""),
-         BNP_endring=na_if(BNP_endring, ""),
+  mutate(BNP=str_replace_all(BNP, " ", ""), # erstatter vi mellomrom som tusenskilletegn
+         BNP_endring=na_if(BNP_endring, ""), #  tomme tegn ("“) for endringene med manglende observasjon (NA)
          BNP_percap_endring=na_if(BNP_percap_endring, ""),
-         BNP_endring=str_replace(BNP_endring, ",","."),
+         BNP_endring=str_replace(BNP_endring, ",","."), # erstatter , som desimalskilletegn med .
          BNP_percap_endring=str_replace(BNP_percap_endring, ",",".")) %>% 
   mutate_if(is.character, as.numeric)
 
@@ -47,6 +50,8 @@ tabell
 
 
 ## ----echo=TRUE, eval=FALSE-------------------------------------------------------------------------------------------------------
-getwd()
-setwd("~/undervisning/SOK1004")
-knitr::purl("case_2_bnp_historisk.Rmd")
+## ----setup, include=FALSE--------------------------------------------------------------------------------------------------------
+#knitr::opts_chunk$set(echo = TRUE)
+#knitr::opts_chunk$set(comment=NA)
+
+
